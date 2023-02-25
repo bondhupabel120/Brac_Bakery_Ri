40c1ca39bf67bf33fb4db69bc6e92172a78fb8ce
Hanif Mia Fabel <bondhupabel@gmail.com>
2023-02-26 02:22:06 +0600

    cookies folder in views folder & image add in cookies notify

---
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -0,0 +1,22 @@
+<div class="js-cookie-consent cookie-consent fixed bottom-0 inset-x-0 pb-2">
+    <div class="max-w-7xl mx-auto px-6">
+        <div class="p-2 rounded-lg bg-yellow-100">
+            <div class="flex items-center justify-between flex-wrap">
+                <div class="w-0 flex-1 items-center hidden md:inline">
+                    <img style="height: 150px" class="img-fluid rounded mb-3" src="{{ asset ('img/carousel-1.jpg')}}" alt="">
+                    <p class="ml-3 text-black cookie-consent__message">
+                        {!! trans('cookie-consent::texts.message') !!}
+                    </p>
+                </div>
+                <div class="mt-2 flex-shrink-0 w-full sm:mt-0 sm:w-auto">
+                    <button class="js-cookie-consent-agree btn-danger cookie-consent__agree cursor-pointer flex items-center justify-center px-4 py-2 rounded-md text-sm font-medium text-yellow-800 bg-yellow-400 hover:bg-yellow-300">
+                        Reject
+                    </button>
+                    <button class="js-cookie-consent-agree btn-info cookie-consent__agree cursor-pointer flex items-center justify-center px-4 py-2 rounded-md text-sm font-medium text-yellow-800 bg-yellow-400 hover:bg-yellow-300">
+                        {{ trans('cookie-consent::texts.agree') }}
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
